7c5dff0ab99ed11f57e2ba8dfead6d179410be8d
Aeron Red <aeronredcelajes@gmail.com>
2025-02-06 03:35:02 +0800

    web portfolio commit #4

--- js/script.js
+++ js/script.js
@@ -1,20 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let dropdown_open = false;
-    
-    function dropDownFunc() {
-      var menu = document.getElementById("headerDropdown");
-  
-      if (window.innerWidth <= 768) {
-        if (!dropdown_open) {
-          menu.style.top = "55px";
-          dropdown_open = true;
-        } else {
-          menu.style.top = "-" + menu.offsetHeight + "px";
-          dropdown_open = false;
-        }
-      }
-    }
-  
+
     // Text Scramble
     const dev_type = ["Web Development", "Computer Vision", "Machine Learning", "Graphic Design ", "Video Editing", "Programmer"];
     const el = document.querySelector("#devType");
@@ -48,4 +33,23 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Start the header animation
     printHomeHeader();
-  });  
+
+    // Tab Active Pane
+    // Get all the tab buttons and tab content elements
+    const tabButtons = document.querySelectorAll('.tab-button');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    // Add click event listener to each tab button
+    tabButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        // Remove the 'active' class from all buttons and tab contents
+        tabButtons.forEach(btn => btn.classList.remove('active'));
+        tabContents.forEach(content => content.classList.remove('active'));
+
+        // Add 'active' class to the clicked tab button and corresponding content
+        button.classList.add('active');
+        const targetContent = document.querySelector(`#${button.id.replace('tab', 'content')}`);
+        targetContent.classList.add('active');
+      });
+    });
+});
